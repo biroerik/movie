1259b52fb34bef0bf4c1026b193cb6c7c1a2eb17
Bíró Erik <biro.erik00@gmail.com>
2023-04-04 14:14:14 +0200

    tests

--- movie-api/src/tests/index.test.ts
+++ movie-api/src/tests/index.test.ts
@@ -1,7 +1,15 @@
-import request from "supertest";
 import index from "../index";
+import request from "supertest";
+
 it("response has correct fields", async () => {
   const response = await request(index).post("/movies").send({ search: "old" });
 
-  expect(response.body).toHaveProperty("results");
+  expect(response.body).not.toBeNull();
+});
+
+it("should listen on the port specified in the environment variable or 8080", () => {
+  const spy = jest.spyOn(console, "log").mockImplementation(() => {});
+  index.listen(8080);
+  expect(spy).toHaveBeenCalledWith("Example app listening on port 8080");
+  spy.mockRestore();
 });
